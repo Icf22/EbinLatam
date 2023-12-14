145abd6b862610a169a5c0d20767372b873be03e
Icf22 <icarrazco@eglobal.com.mx>
2023-12-14 12:13:03 -0600

    Flujos Simple Consultas Aceptadas y rechazadas

--- POM/tests/Ventas.spec.ts
+++ POM/tests/Ventas.spec.ts
@@ -4,7 +4,28 @@ import {VentasPage} from "../pages/ventas.page";
 
 test.use({ignoreHTTPSErrors: true});
 
-test('consultaVentasAceptadasEglobal', async ({page}) => {
+test.only('consultaVentasAceptadasEglobalSimple', async ({page}) => {
+    const basePage = new BasePage(page);
+    const ventasPage = new VentasPage(page);
+    await basePage.iniciarSesion('Eglobal');
+    await basePage.menuConsultaVentas();
+    await ventasPage.consultarVentasAceptadasEglobal();
+    //await ventasPage.validarFuncionalidades();
+    await basePage.cerrarSesion();
+})
+
+test.only('consultaVentasRechazadasEglobalSimple', async ({page}) => {
+    const basePage = new BasePage(page);
+    const ventasPage = new VentasPage(page);
+    await basePage.iniciarSesion('Eglobal');
+    await basePage.menuConsultaVentas();
+    await ventasPage.consultarVentasRechazadasEglobal();
+    //await ventasPage.validarFuncionalidades();
+    await basePage.cerrarSesion();
+})
+
+
+test('consultaVentasAceptadasEglobalFullValidation', async ({page}) => {
     const basePage = new BasePage(page);
     const ventasPage = new VentasPage(page);
     await basePage.iniciarSesion('Eglobal');
@@ -13,7 +34,7 @@ test('consultaVentasAceptadasEglobal', async ({page}) => {
     await ventasPage.validarFuncionalidades();
     await basePage.cerrarSesion();
 })
-test('consultaVentasAceptadasAdquiriente', async ({page}) => {
+test('consultaVentasAceptadasAdquirienteFullValidation', async ({page}) => {
     const basePage = new BasePage(page);
     const ventasPage = new VentasPage(page);
     await basePage.iniciarSesion('Adquiriente');
--- POM/tests/gestionControversias.spec.ts
+++ POM/tests/gestionControversias.spec.ts
@@ -4,7 +4,7 @@ import {BasePage} from "../pages/base.page"
 
 test.use({ignoreHTTPSErrors: true});
 
-test('consultaControversias', async ({page}) => {
+test.only('consultaControversias', async ({page}) => {
    const controversias = new ControversiasPage(page);
    const basePage = new BasePage(page);
    await basePage.iniciarSesion('Eglobal');
@@ -14,7 +14,7 @@ test('consultaControversias', async ({page}) => {
 })
 
 
-test.only('altaControversias',async ({page}) => {
+test('altaControversias',async ({page}) => {
    const controversias = new ControversiasPage(page);
    const basePage = new BasePage(page);
    await basePage.iniciarSesion('Eglobal');
